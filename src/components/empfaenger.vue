b9d8c5c87961e3d936e50f67804002d9f2c24854
Finn <fhertsch235@gmail.com>
2024-06-12 10:24:24 +0200

    Form submit client-side

--- src/components/empfaenger.vue
+++ src/components/empfaenger.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div class="form-container">
-        <form action="">
+        <form action="" @submit.prevent="handleEmpfaengerForm(), checkGenderValue()">
             <h3>Empfänger</h3>
             <label for="schuleName">
             <label for="lehrerName">
@@ -11,8 +11,9 @@
             <label for="lehrerGender">
                 <p>Geschlecht des Lehrers</p>
             </label>
-            <input class="check-input" type="checkbox" name="weiblich" id="wCeckbox"> weiblich
-            <input class="check-input" type="checkbox" name="maennlich" id="mCeckbox"> männlich
+            <input class="check-input" type="checkbox" name="weiblich" id="wCeckbox" v-model="newLehrerFemale"> weiblich
+            <input class="check-input" type="checkbox" name="maennlich" id="mCeckbox" v-model="newLehrerMale"> männlich
+            <p v-if="genderValueIssue">Bitte nur ein Geschlecht auswählen!</p>
             <p>Name der Schule</p>
             </label> 
             <input class="text-input" type="text" name="schuleName" v-model="newSchuleName">
@@ -30,6 +31,7 @@
                     <input class="stadt-input" type="text" name="schuleStadt" v-model="newSchuleStadt">
                 </div>
             </div>
+            <input type="submit" value="Absenden"> 
         </form>
     </div>
 </template>
@@ -38,28 +40,68 @@
     import '@/assets/css/form.css'
     import { RouterLink, RouterView } from "vue-router";
     import { onMounted, ref } from "vue";
+    import axios from "axios";
 
     const newSchuleName = ref("");
     const newLehrerName = ref("");
-    const newLehrerGender = ref("");
+    const newLehrerFemale = ref(false);
+    const newLehrerMale = ref(true);
     const newSchuleAdresse = ref("");
     const newSchuleStadt =  ref("");
     const newSchulePlz =  ref("");
 
-    const lehrerName = newLehrerName.value
-    const lehrerGender = newLehrerGender.value
-    const schuleName = newSchuleName.value
-    const schuleAdresse = newSchuleAdresse.value
-    const schulePlz = newSchulePlz.value
-    const schuleStadt = newSchuleStadt.value
+    let genderValueIssue = false;
+    let formSubmitted = false;
 
-    const empfaenger = {
+    const checkGenderValue = () => {
+
+        if (newLehrerFemale.value && newLehrerMale.value == true) {
+            genderValueIssue = true;
+        }
+        
+        console.log("Gender issue")
+
+    };
+
+
+    const handleEmpfaengerForm = () => {
+        const lehrerName = newLehrerName.value
+        const lehrerFemale = newLehrerFemale.value
+        const lehrerMale = newLehrerMale.value
+        const schuleName = newSchuleName.value
+        const schuleAdresse = newSchuleAdresse.value
+        const schulePlz = newSchulePlz.value
+        const schuleStadt = newSchuleStadt.value
+
+        const empfaenger = {
         lehrerName: lehrerName,
-        lehrerGender: lehrerGender,
+        lehrerFemale: lehrerFemale,
+        lehrerMale: lehrerMale,
         schuleName: schuleName,
         schuleAdresse: schuleAdresse,
         schulePlz: schulePlz,
         schuleStadt: schuleStadt
+        }
+        //sendFormData(empfaenger);
+        formSubmitted = true;
+        console.log("Email übergeben", empfaenger)
+    }
+
+    const sendFormData = async (empfaenger) => {
+        try {
+            const response = await axios.post('url', empfaenger);
+            if (response.status === 200) {
+            console.log("Übertragung erfolgreich");
+            resetSubmit();
+            }
+            else {
+            alert('Etwas ist schiefgelaufen.');
+            }
+        }
+        catch (error) {
+            console.error('Error: ', error);
+            alert('Fehler aufgetreten.', error);
+        }
     }
 
 </script>
